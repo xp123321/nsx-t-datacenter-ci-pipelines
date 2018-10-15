08e492c91d5f457f020ad3ae2ceaaa61f0b46d6f
Kai Su <skai@vmware.com>
2018-10-15 13:14:18 -0700

    Add proxy settings to container env vars
    Signed-off-by: Kai Su <skai@vmware.com>

--- docker_image/run.sh
+++ docker_image/run.sh
@@ -84,10 +84,23 @@ sed -i "0,/^ *- CONCOURSE_NO_REALLY_I_DONT_WANT_ANY_AUTH/ s|CONCOURSE_NO_REALLY_
 sed  -i "/^ *image: concourse\/concourse/ s|concourse/concourse.*$|concourse/concourse:$concourse_version|g" docker-compose.yml
 
 # remove lines containing following parameters
-patterns=("CONCOURSE_BASIC_AUTH_USERNAME" "CONCOURSE_BASIC_AUTH_PASSWORD" "http_proxy_url" "https_proxy_url" "no_proxy" "HTTP_PROXY" "HTTPS_PROXY" "NO_PROXY")
+patterns=("CONCOURSE_BASIC_AUTH_USERNAME" "CONCOURSE_BASIC_AUTH_PASSWORD" "http_proxy_url" "https_proxy_url" "no_proxy")
 for p in "${patterns[@]}"; do
         sed -i "/$p/d" docker-compose.yml
 done
+
+# If proxy env vars not set, remove the settings
+proxy_patterns=("HTTP_PROXY" "HTTPS_PROXY" "NO_PROXY")
+if [[ -z "$HTTP_PROXY" ]] || [[ -z "HTTPS_PROXY" ]]; then
+	for p in in "${proxy_patterns[@]}"; do
+		sed -i "/$p/d" docker-compose.yml
+	done
+else
+	sed -i "0,/^ *- HTTP_PROXY/ s|HTTP_PROXY.*$|HTTP_PROXY=${HTTP_PROXY}|" docker-compose.yml
+	sed -i "0,/^ *- HTTPS_PROXY/ s|HTTPS_PROXY.*$|HTTPS_PROXY=${HTTPS_PROXY}|" docker-compose.yml
+	sed -i "0,/^ *- NO_PROXY/ s|NO_PROXY.*$|NO_PROXY=${NO_PROXY}|" docker-compose.yml
+fi
+
 #sed -i "0,/^ *- CONCOURSE_GARDEN_NETWORK/ s|- CONCOURSE_GARDEN_NETWORK.*$|#- CONCOURSE_GARDEN_NETWORK|" docker-compose.yml
 #sed -i "/^ *- CONCOURSE_EXTERNAL_URL/ a\    - CONCOURSE_NO_REALLY_I_DONT_WANT_ANY_AUTH=true" docker-compose.yml
 
